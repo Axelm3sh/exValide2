9040222b7f9061a3e9351d68729887211b64d674
Axelm3sh <axel.m3sh@gmail.com>
2017-06-01 10:59:40 -0700

    new proj, refactor

--- exValide2/proj.android/src/org/oxygine/ExValide2/MainActivity.java
+++
@@ -1,8 +0,0 @@
-package org.oxygine.ExValide2;
-
-import org.oxygine.lib.OxygineActivity;
-
-public class MainActivity extends OxygineActivity
-{
-
-}
---
+++ my_awesome_game/gen-atlasses-android(ETC1).bat
@@ -0,0 +1,1 @@
+..\oxygine-framework\tools\oxyresbuild -x xmls\ui.xml --src_data data --dest_data data --compress etc1
